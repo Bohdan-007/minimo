dde2707d6be6edbb79f50ac2101d376ab3233875
Bohdan-007 <elsetech133718@gmail.com>
2023-02-22 20:38:15 +0200

    added main content

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// mobile sidenav
 $(document).ready(function () {
   $('.sidenav').sidenav();
+});
+
+// carousel
+$(document).ready(function () {
+  $('.slider').slider({
+    indicators: false,
+    height: 675,
+    // indicators: true
+  });
 });
